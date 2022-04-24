7bb7620c13e018ede5c5a29588fa7a0c268eb034
reubentriodash <reubent.triodash@gmail.com>
2022-04-24 14:15:45 +0545

    few updates

--- config/database.js
+++ config/database.js
@@ -1,19 +1,11 @@
-const parse = require('pg-connection-string').parse;
-    const config = parse(process.env.DATABASE_URL);
-    module.exports = ({ env }) => ({
-      connection: {
-        client: 'postgres',
-        connection: {
-          host: config.host,
-          port: config.port,
-          database: config.database,
-          user: config.user,
-          password: config.password,
-          ssl: {
-            rejectUnauthorized: false
-          },
-        },
-        debug: false,
-      },
-    });
-    
+const path = require('path');
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+    },
+    useNullAsDefault: true,
+  },
+});
